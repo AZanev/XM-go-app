abe1c47203693a3c8d31f22602b3b669de3f837e
AZanev <38461087+AZanev@users.noreply.github.com>
2023-03-24 17:15:51 +0200

    added main.go
    Added main.go in order toDockerfile to function properly if tested

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	for{
+		http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"Hello" : "world!"}`))
+		})
+
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatalf("error: %v", err)
+		}
+	}
+	
+}
